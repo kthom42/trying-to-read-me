b056b3ab4d87745fd5cfa2e2057959e7f49830c5
kthom42 <kmgt42@gmail.com>
2022-11-21 23:45:19 -0500

    added questions to generator

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Give a description of your project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Usage details",
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "List contributors",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your github url",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "Choose a license",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
